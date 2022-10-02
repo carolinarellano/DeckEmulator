ae7bd6830b2b93a860675ef40021265b8b360471
Carolina Arellano <97354399+carolinarellano@users.noreply.github.com>
2022-10-01 23:28:25 -0500

    Last commit

--- main.c
+++ main.c
@@ -10,7 +10,10 @@ typedef struct deck{
 
 void print_char_array(char *arr, size_t size);
 Deck pull(Deck *my_deck, int *count);
-//Falta implementar la funcion para almacenar en el archivo (funcion para save, stop) [Supongo que va desde el main]
+//Falta almacenar en el archivo (funcion para save, stop)
+void save(Deck *my_deck, int *count);
+void load();
+int stop();
 
 int main() {
 
@@ -39,9 +42,9 @@ int main() {
     mi_deck.cards[3] = numbers[3];
     mi_deck.cards[4] = numbers[4];
 
-    //mi_deck.hand = mi_deck.cards[count - 1];
 
     print_char_array(mi_deck.cards, count);
+    //save(&mi_deck, &count);
     while(count > 0){
         pull(&mi_deck, &count);
     }
@@ -56,14 +59,19 @@ void print_char_array(char *arr, size_t size) {
     } else {
         putchar('[');
         for (int i = 0; i < size - 1; i++)
-            if(arr[i] < 65)
+            if(arr[i] == 10)
             {
                 printf("%d, ", arr[i]);
             }
             else{
                 printf("%c, ", arr[i]);
             }
-        printf("%c] ", arr[size - 1]);
+        if(arr[size - 1] == 10){
+            printf("%d] ", arr[size - 1]);
+        }else{
+            printf("%c] ", arr[size - 1]);
+        }
+
     }
 }
 
@@ -74,7 +82,12 @@ Deck pull(Deck *my_deck, int *count)
 
     my_deck->hand = my_deck->cards[*count- 1];
 
-    printf("Your current card is: <%c>", my_deck->hand);
+    if(my_deck->hand == 10){
+        printf("Your current card is: <%d>", my_deck->hand);
+    }else{
+        printf("Your current card is: <%c>", my_deck->hand);
+    }
+
 
     printf("\nWhat do you want to do with your current card?\n");
     printf("If you want to take it back to the top of the deck -> t\n"
@@ -89,6 +102,7 @@ Deck pull(Deck *my_deck, int *count)
             //printf("<%c>\n", my_deck->hand);
             my_deck->cards[*count-1] = my_deck->hand;
             print_char_array(my_deck->cards, *count);
+            save(my_deck, count);
             break;
 
         case ('b'):
@@ -101,12 +115,23 @@ Deck pull(Deck *my_deck, int *count)
             my_deck->cards[0] = temp[0];
             print_char_array(my_deck->cards, *count);
             printf("\n");
+            save(my_deck, count);
             break;
 
         case ('d'):
             my_deck->hand = ' ';
             print_char_array(my_deck->cards, *count - 1);
             *count -= 1;
+            save(my_deck, count);
+            break;
+
+        case ('s'):
+            save(my_deck, count);
+            printf("Saved!\n");
+            exit(0);
+
+        case ('e'):
+            stop();
             break;
 
         default:
@@ -114,5 +139,40 @@ Deck pull(Deck *my_deck, int *count)
             fgetc(stdin);
             fgetc(stdin);
             print_char_array(my_deck->cards, *count);
+            break;
     }
+}
+
+void save(Deck *my_deck, int *count){
+    FILE *fptr;
+    fptr = fopen("..\\Deck", "a+");
+    if(fptr == NULL){
+        printf("ERROR");
+    }
+    for(int i = 0; i < *count; i++){
+        if(my_deck->cards[i] < 65)
+        {
+            fprintf(fptr, "%d ", my_deck->cards[i]);
+        }
+        else{
+            fprintf(fptr, "%c ", my_deck->cards[i]);
+        }
+    }
+    if(my_deck->hand == 10){
+        fprintf(fptr, "<%d>\n", my_deck->hand);
+
+    }else{
+        fprintf(fptr, "<%c>\n", my_deck->hand);
+    }
+}
+
+void load(){
+    FILE *fptr;
+    fptr = fopen("..\\Deck", "r+");
+    if(fptr == NULL){
+        printf("There's not a game saved before");
+    }
+}
+int stop(){
+    exit(0);
 }
